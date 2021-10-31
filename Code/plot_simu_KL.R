5139b3f73858af4365fcca4cecc43e309119576d
YunyiShen <yshen99@wisc.edu>
2021-10-30 23:42:28 -0500

    more results on KL

--- Code/plot_simu_KL.R
+++ Code/plot_simu_KL.R
@@ -1,12 +1,13 @@
 library(ggplot2)
 library(reshape2)
 
-res_dir <- "./Res/init_200_stepsize_50_steps_40_lambda_kp1_stein"
+res_dir <- "./Res/init_200_stepsize_50_steps_40_lambda_kp1_kl"
 res_files <- list.files(res_dir, pattern = "csv$", full.names = T)
-res_files <- res_files[-grep("spc",res_files)]
+#res_files <- res_files[-grep("Model5",res_files)]
+res_files <- res_files[grep("spc",res_files)]
 res_df <- lapply(res_files,read.csv)
 
-res_mean <- sapply(res_df, colMeans)
+res_mean <- sapply(res_df, colMeans, na.rm = T)
 colnames(res_mean) <- c("MGIG-cert-1", "MGIG-uncert-1",
                         "Wishart-cert-1","Wishart-uncert-1",
                         "MGIG-cert-2", "MGIG-uncert-2",
@@ -16,25 +17,26 @@ colnames(res_mean) <- c("MGIG-cert-1", "MGIG-uncert-1",
                         
                         "MGIG-cert-4", "MGIG-uncert-4",
                         "Wishart-cert-4","Wishart-uncert-4",
-                        "MGIG-cert-5", "MGIG-uncert-5",
                         
+                        "MGIG-cert-5", "MGIG-uncert-5",
                         "Wishart-cert-5","Wishart-uncert-5",
+                        
                         "MGIG-cert-6", "MGIG-uncert-6",
                         "Wishart-cert-6","Wishart-uncert-6"
                         )
 
 res_lw <- sapply(res_df, function(w){
-  apply(as.matrix(w),2, quantile, probs = 0.025)
+  apply(as.matrix(w),2, quantile, probs = 0.025, na.rm = T)
 })
 colnames(res_lw) <- colnames(res_mean)
 
 res_hi <- sapply(res_df, function(w){
-  apply(as.matrix(w),2, quantile, probs = 0.925)
+  apply(as.matrix(w),2, quantile, probs = 0.925, na.rm = T)
 })
 
 colnames(res_hi) <- colnames(res_mean)
 
-sample_size <- 200 + 0:40 * 50
+sample_size <- 200 + 0:39 * 50
 
 res_mean <- data.frame(sample_size=sample_size, res_mean)
 res_lw <- data.frame(sample_size=sample_size, res_lw)
@@ -55,14 +57,14 @@ res_hi_long$model <- res_mean_long$model
 plot_data <- merge(res_mean_long, res_hi_long)
 plot_data <- merge(plot_data, res_lw_long)
 
-ggplot(data=plot_data, aes(x=sample_size, y = mean, color = prior, shape = prior)) + 
+ggplot(data=plot_data, aes(x=sample_size, y = log(mean), color = prior, shape = prior)) + 
   geom_hline(yintercept =  0, lty = 2) + 
   geom_line() + 
-  geom_errorbar(aes(ymin = lw, ymax = hi), alpha = 0.3, width = 5) + 
+  geom_errorbar(aes(ymin = log(lw), ymax = log(hi)), alpha = 0.3, width = 5) + 
   scale_color_manual(values = c("#009E73", "#0072B2", "#D55E00", "#CC79A7")) + 
   xlab("Sample size") + 
-  ylab("log Stein's loss difference to no experiment") + 
-  facet_wrap(~model, nrow = 3)
+  ylab("log KL divergence to prior difference compare to no experiment") + 
+  facet_wrap(~model, nrow = 3, scales = "free_y")
 
-ggsave("./Res/init_200_stepsize_50_steps_40_lambda_kp1_stein/log_stein_diff3_rand_exp.pdf",
+ggsave("./Res/init_200_stepsize_50_steps_40_lambda_kp1_kl/log_KL_diff2_spc_trt.pdf",
        width = 10, height = 8, scale = 0.8)
